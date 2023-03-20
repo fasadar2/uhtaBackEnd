c001cc06b82967f343301e6c02c40fcec56cce89
fasadar2 <orlov.maksim.02@gmail.com>
2023-03-20 18:23:55 +0300

    first commit add Readme

--- src/main/java/com/example/uhta/model/requestModel/requestModel.java
+++
@@ -1,9 +0,0 @@
-package com.example.uhta.model.requestModel;
-
-public class requestModel extends basicRequestModel{
-    public requestModel(int code, String message) {
-        super(code, message);
-    }
-    public requestModel(){}
-
-}
