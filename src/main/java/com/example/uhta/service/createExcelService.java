34636ed90257b03554c58154f02c92ed504d10ce
orlov <orlov.maksim.02@gmail.com>
2023-05-23 12:39:59 +0300

    Add excel generation

--- src/main/java/com/example/uhta/service/createExcelService.java
+++ src/main/java/com/example/uhta/service/createExcelService.java
@@ -7,55 +7,46 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.springframework.stereotype.Service;
 
 import java.io.FileOutputStream;
+import java.lang.reflect.Field;
 import java.util.List;
 
 @Service
 public class createExcelService {
-    public void createSheet(List<PdfModel> pdf){
-        try
-        {
-//declare file name to be create
-            String filename = "F:\\desktop\\uhta\\Отчет.xls";
-//creating an instance of HSSFWorkbook class
+    public void createSheet(List<PdfModel> pdf) {
+        try {
             HSSFWorkbook workbook = new HSSFWorkbook();
-//invoking creatSheet() method and passing the name of the sheet to be created
             HSSFSheet sheet = workbook.createSheet("January");
-//creating the 0th row using the createRow() method
-            HSSFRow rowhead = sheet.createRow((short)0);
-//creating cell by using the createCell() method and setting the values to the cell by using the setCellValue() method
-            rowhead.createCell(0).setCellValue("S.No.");
-            rowhead.createCell(1).setCellValue("Customer Name");
-            rowhead.createCell(2).setCellValue("Account Number");
-            rowhead.createCell(3).setCellValue("e-mail");
-            rowhead.createCell(4).setCellValue("Balance");
-//creating the 1st row
-            HSSFRow row = sheet.createRow((short)1);
-//inserting data in the first row
-            row.createCell(0).setCellValue("1");
-            row.createCell(1).setCellValue("John William");
-            row.createCell(2).setCellValue("9999999");
-            row.createCell(3).setCellValue("william.john@gmail.com");
-            row.createCell(4).setCellValue("700000.00");
-//creating the 2nd row
-            HSSFRow row1 = sheet.createRow((short)2);
-//inserting data in the second row
-            row1.createCell(0).setCellValue("2");
-            row1.createCell(1).setCellValue("Mathew Parker");
-            row1.createCell(2).setCellValue("22222222");
-            row1.createCell(3).setCellValue("parker.mathew@gmail.com");
-            row1.createCell(4).setCellValue("200000.00");
+
+            Field[] fields = pdf.get(0).getClass().getDeclaredFields();
+
+
+            HSSFRow rowHeads = sheet.createRow((short) 0);
+            for(int i = 0; i < fields.length; i++)
+                rowHeads.createCell(i).setCellValue(fields[i].getName());
+
+            for(int i = 0; i < pdf.size(); i++) {
+                HSSFRow hssfRow = sheet.createRow(i + 1);
+                hssfRow.createCell(0).setCellValue(pdf.get(i).getDescription());
+                hssfRow.createCell(1).setCellValue(pdf.get(i).getAssetType());
+                hssfRow.createCell(2).setCellValue(pdf.get(i).getWeight());
+                hssfRow.createCell(3).setCellValue(pdf.get(i).getOscillationIndex());
+                hssfRow.createCell(4).setCellValue(pdf.get(i).getServiceFactor());
+                hssfRow.createCell(5).setCellValue(pdf.get(i).getEffectiveServiceFactor());
+                hssfRow.createCell(6).setCellValue(pdf.get(i).getDisspositionFlaggedDate());
+                hssfRow.createCell(7).setCellValue(pdf.get(i).getDisspositionEntryDate());
+                hssfRow.createCell(8).setCellValue(pdf.get(i).getDisposition());
+                hssfRow.createCell(9).setCellValue(pdf.get(i).getWeeksInCurrentDisposition());
+                hssfRow.createCell(10).setCellValue(String.join("\n", pdf.get(i).getDispositionComment()));
+            }
+
+            String filename = "F:/documents/Отчет.xls";
             FileOutputStream fileOut = new FileOutputStream(filename);
             workbook.write(fileOut);
-//closing the Stream
             fileOut.close();
-//closing the workbook
             workbook.close();
 
-//prints the message on the console
             System.out.println("Excel file has been generated successfully.");
-        }
-        catch (Exception e)
-        {
+        } catch(Exception e) {
             e.printStackTrace();
         }
     }
