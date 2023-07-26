8a689297ef28742f06a951464333fd02671d53a8
LoXur-ux <starkaw@ya.ru>
2023-07-26 17:44:24 +0300

    Fix Report File Name

--- src/main/java/com/example/uhta/service/CreateExcelService.java
+++ src/main/java/com/example/uhta/service/CreateExcelService.java
@@ -13,58 +13,58 @@ import java.util.List;
 
 @Service
 public class CreateExcelService {
-    public String createSheet(List<PdfModel> pdf) {
-        Date dateDate = new Date();
-        String dateString = dateDate.toString();
-        String filename = "Report.xls"+dateString;
-        try {
-            HSSFWorkbook workbook = new HSSFWorkbook();
-            HSSFSheet sheet = workbook.createSheet("January");
+	public String createSheet(List<PdfModel> pdf) {
+		String name = new Date().toString().replace(" ", "_").replace(":", "-");
 
-            Field[] fields = pdf.get(0).getClass().getDeclaredFields();
+		String filename = "Report_" + name + ".xls";
 
-            HSSFRow rowHeads = sheet.createRow((short) 0);
-            for(int i = 0; i < fields.length; i++)
-                rowHeads.createCell(i).setCellValue(fields[i].getName());
+		try {
+			HSSFWorkbook workbook = new HSSFWorkbook();
+			HSSFSheet sheet = workbook.createSheet("January");
 
-            for(int i = 0; i < pdf.size(); i++) {
-                // TODO: Сделать notNull
-                HSSFRow hssfRow = sheet.createRow(i + 1);
-                hssfRow.createCell(0).setCellValue(pdf.get(i).getName());
-                hssfRow.createCell(1).setCellValue(pdf.get(i).getAssetType());
-                if(pdf.get(i).getAttributesContains().contains("Weight"))
-                    hssfRow.createCell(2).setCellValue(pdf.get(i).getWeight());
-                if(pdf.get(i).getAttributesContains().contains("OscillationIndex"))
-                    hssfRow.createCell(3).setCellValue(pdf.get(i).getOscillationIndex());
-                if(pdf.get(i).getAttributesContains().contains("ServiceFactor"))
-                    hssfRow.createCell(4).setCellValue(pdf.get(i).getServiceFactor());
-                if(pdf.get(i).getAttributesContains().contains("EffectiveServiceFactor"))
-                    hssfRow.createCell(5).setCellValue(pdf.get(i).getEffectiveServiceFactor());
-                if(pdf.get(i).getAttributesContains().contains("DispositionFlaggedDate"))
-                    hssfRow.createCell(6).setCellValue(pdf.get(i).getDisspositionFlaggedDate());
-                if(pdf.get(i).getAttributesContains().contains("DispositionEntryDate"))
-                    hssfRow.createCell(7).setCellValue(pdf.get(i).getDisspositionEntryDate());
-                if(pdf.get(i).getAttributesContains().contains("Disposition"))
-                    hssfRow.createCell(8).setCellValue(pdf.get(i).getDisposition());
-                if(pdf.get(i).getAttributesContains().contains("WeeksInCurrentDisposition"))
-                    hssfRow.createCell(9).setCellValue(pdf.get(i).getWeeksInCurrentDisposition());
-                if (pdf.get(i).getAttributesContains().contains("DispositionComment"))
-                    hssfRow.createCell(10).setCellValue(String.join("\n", pdf.get(i).getDispositionComment()));
+			Field[] fields = pdf.get(0).getClass().getDeclaredFields();
 
-            }
+			HSSFRow rowHeads = sheet.createRow((short) 0);
+			for(int i = 0; i < fields.length; i++)
+				rowHeads.createCell(i).setCellValue(fields[i].getName());
+
+			for(int i = 0; i < pdf.size(); i++) {
+				HSSFRow hssfRow = sheet.createRow(i + 1);
+				hssfRow.createCell(0).setCellValue(pdf.get(i).getName());
+				hssfRow.createCell(1).setCellValue(pdf.get(i).getAssetType());
+				if(pdf.get(i).getAttributesContains().contains("Weight"))
+					hssfRow.createCell(2).setCellValue(pdf.get(i).getWeight());
+				if(pdf.get(i).getAttributesContains().contains("OscillationIndex"))
+					hssfRow.createCell(3).setCellValue(pdf.get(i).getOscillationIndex());
+				if(pdf.get(i).getAttributesContains().contains("ServiceFactor"))
+					hssfRow.createCell(4).setCellValue(pdf.get(i).getServiceFactor());
+				if(pdf.get(i).getAttributesContains().contains("EffectiveServiceFactor"))
+					hssfRow.createCell(5).setCellValue(pdf.get(i).getEffectiveServiceFactor());
+				if(pdf.get(i).getAttributesContains().contains("DispositionFlaggedDate"))
+					hssfRow.createCell(6).setCellValue(pdf.get(i).getDisspositionFlaggedDate());
+				if(pdf.get(i).getAttributesContains().contains("DispositionEntryDate"))
+					hssfRow.createCell(7).setCellValue(pdf.get(i).getDisspositionEntryDate());
+				if(pdf.get(i).getAttributesContains().contains("Disposition"))
+					hssfRow.createCell(8).setCellValue(pdf.get(i).getDisposition());
+				if(pdf.get(i).getAttributesContains().contains("WeeksInCurrentDisposition"))
+					hssfRow.createCell(9).setCellValue(pdf.get(i).getWeeksInCurrentDisposition());
+				if(pdf.get(i).getAttributesContains().contains("DispositionComment"))
+					hssfRow.createCell(10).setCellValue(String.join("\n", pdf.get(i).getDispositionComment()));
+
+			}
 
 
-            FileOutputStream fileOut = new FileOutputStream(filename);
-            workbook.write(fileOut);
-            fileOut.close();
-            workbook.close();
+			FileOutputStream fileOut = new FileOutputStream(filename);
+			workbook.write(fileOut);
+			fileOut.close();
+			workbook.close();
 
-            System.out.println("Excel file has been generated successfully.");
+			System.out.println("Excel file has been generated successfully.");
 
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
-        return filename;
-    }
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return filename;
+	}
 
 }
